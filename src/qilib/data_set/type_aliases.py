d8e7c5baa1e9170f0b44fe333b96180b9b792558
qSaevar <saevar.steinn@gmail.com>
2019-05-09 16:19:44 +0200

    Typehinting the PythonJsonStructure

--- src/qilib/data_set/type_aliases.py
+++ src/qilib/data_set/type_aliases.py
@@ -7,5 +7,3 @@ DataArrays = Union[List[DataArray], Tuple[DataArray, ...], DataArray]
 PJSValueTypes = Union[None, bool, int, float, complex, str, bytes]
 PJSContainers = Sequence[PJSValueTypes]
 PJSValues = Union[PJSContainers, PJSValueTypes]
-    # __serializable_container_types = (list, tuple, np.ndarray)
-    # __serializable_value_types = (type(None), bool, int, float, complex, str, bytes)
