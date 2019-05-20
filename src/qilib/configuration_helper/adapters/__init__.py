2714b89aa25791006b6cfa74a9fcb5955ef68d69
qSaevar <39056113+qSaevar@users.noreply.github.com>
2019-05-20 16:58:46 +0200

    [DEM-940] Implement instrument adapter for keysight vector source (#21)

--- src/qilib/configuration_helper/adapters/__init__.py
+++ src/qilib/configuration_helper/adapters/__init__.py
@@ -8,3 +8,4 @@ from qilib.configuration_helper.adapters.hdawg8_instrument_adapter import ZIHDAW
 from qilib.configuration_helper.adapters.m2j_instrument_adapter import M2jInstrumentAdapter
 from qilib.configuration_helper.adapters.s5i_instrument_adapter import S5iInstrumentAdapter
 from qilib.configuration_helper.adapters.uhfli_instrument_adapter import ZIUHFLIInstrumentAdapter
+from qilib.configuration_helper.adapters.keysight_e8267d_instrument_adapter import KeysightE8267DInstrumentAdapter
