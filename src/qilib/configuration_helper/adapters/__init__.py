6026138f2d5282cd6fe3ba0e49bcf03b4df0785f
qSaevar <39056113+qSaevar@users.noreply.github.com>
2019-06-05 15:40:58 +0200

    [DEM-976] Create TimeStampInstrumentAdapter (#29)

--- src/qilib/configuration_helper/adapters/__init__.py
+++ src/qilib/configuration_helper/adapters/__init__.py
@@ -1,3 +1,4 @@
+from qilib.configuration_helper import InstrumentAdapterFactory
 from qilib.configuration_helper.adapters.ami430_instrument_adapter import AMI430InstrumentAdapter
 from qilib.configuration_helper.adapters.common_instrument_adapter import CommonInstrumentAdapter
 from qilib.configuration_helper.adapters.keithley_dmm6500_adapter import Keithley6500InstrumentAdapter
@@ -9,5 +10,10 @@ from qilib.configuration_helper.adapters.f1d_instrument_adapter import F1dInstru
 from qilib.configuration_helper.adapters.hdawg8_instrument_adapter import ZIHDAWG8InstrumentAdapter
 from qilib.configuration_helper.adapters.m2j_instrument_adapter import M2jInstrumentAdapter
 from qilib.configuration_helper.adapters.s5i_instrument_adapter import S5iInstrumentAdapter
+try:
+    from qilib.configuration_helper.adapters.time_stamp_instrument_adapter import TimeStampInstrumentAdapter
+except ImportError as e:
+    InstrumentAdapterFactory.failed_adapters['TimeStampInstrumentAdapter'] = e
 from qilib.configuration_helper.adapters.uhfli_instrument_adapter import ZIUHFLIInstrumentAdapter
 from qilib.configuration_helper.adapters.keysight_e8267d_instrument_adapter import KeysightE8267DInstrumentAdapter
+
