205c75674dd5688d7aa737aa1cfcc66f5193eb5d
CumulonimbusCalvus <9318177+CumulonimbusCalvus@users.noreply.github.com>
2019-07-08 16:19:36 +0200

    [DEM-977] Added get instrument to visitor.

--- src/tests/unittests/configuration_helper/test_instrument_configuration_visitor.py
+++ src/tests/unittests/configuration_helper/test_instrument_configuration_visitor.py
@@ -8,6 +8,7 @@ from qilib.utils import PythonJsonStructure
 
 
 class DummyAdapter(CommonInstrumentAdapter):
+
     def __init__(self, address: str):
         super().__init__(address)
         self._instrument = Mock()
@@ -17,6 +18,7 @@ class DummyAdapter(CommonInstrumentAdapter):
 
 
 class TestInstrumentConfigurationVisitor(unittest.TestCase):
+
     def test_visit(self):
         adapter = DummyAdapter('fake')
         with patch('qilib.configuration_helper.instrument_configuration.InstrumentAdapterFactory') as mock_factory:
@@ -26,3 +28,21 @@ class TestInstrumentConfigurationVisitor(unittest.TestCase):
         visitor = InstrumentConfigurationVisitor()
         instrument_configuration_set.accept(visitor)
         self.assertIs(visitor.instruments[0], adapter.instrument)
+
+    def test_get_instrument(self):
+        instrument_class_name = 'DummyClass'
+        instrument_address = 'fake-address-1'
+        instrument_adapter_address = f'{instrument_class_name}_{instrument_address}'
+        adapter = DummyAdapter('fake')
+        adapter._instrument.name = instrument_adapter_address
+        with patch('qilib.configuration_helper.instrument_configuration.InstrumentAdapterFactory') as mock_factory:
+            mock_factory.get_instrument_adapter.return_value = adapter
+            instrument_1 = InstrumentConfiguration(instrument_class_name, instrument_address, Mock(), tag=['instrument_1'])
+        instrument_configuration_set = InstrumentConfigurationSet(Mock(), instruments=[instrument_1])
+        visitor = InstrumentConfigurationVisitor()
+        instrument_configuration_set.accept(visitor)
+
+        self.assertEqual(visitor.get_instrument(instrument_adapter_address), adapter.instrument)
+
+        wrong_address = 'DummyClass_fake_address-2'
+        self.assertRaisesRegex(ValueError, 'No adapter with identifier', visitor.get_instrument, wrong_address)
