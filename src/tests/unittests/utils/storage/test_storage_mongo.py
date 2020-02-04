7aa3001b0692ef2c89565196d6651c41330ab712
Animesh Jaipurkar <animesh.jaipurkar@gmail.com>
2020-02-04 11:09:32 +0100

    Grouped the test cases into different categories

--- src/tests/unittests/utils/storage/test_storage_mongo.py
+++ src/tests/unittests/utils/storage/test_storage_mongo.py
@@ -19,6 +19,29 @@ class TestStorageMongo(unittest.TestCase):
             self.test_data = [10, 3.14, 'string', {'a': 1, 'b': 2}, [1, 2], [1, [2, 3]], {'test': {'test': 2}},
                               {'tuple': (1, 2, 3, 4, 5)}, (1, 2, 3, 4, 5), (1, 2, {'he.llo': 'world'}),
                               {'int64': np.int64(3.)}]
+            self.test_individual_data = {
+                1: 'integer_value',
+                '1': 'string_value',
+                'dict': {
+                    1: 'another_integer_value',
+                    '1': {
+                        1: 'last_integer_value',
+                        'something.with.dot': 'another.key.with.dot'
+                    }
+                },
+                'something.with.dot': 'key.with.dot',
+                'something.else.with.dots': {
+                    'key.dot': 123
+                },
+                'something': {
+                    'with.dot': 'nested values',
+                    'with': {
+                        'dot': 123
+                    }
+                },
+                'li.st': ['is', {'a': 'list', 12: 34}],
+                'tu.ple': (1, 2, 'tuple', (1, 2, 3))
+            }
 
     def tearDown(self) -> None:
         self.storage._collection.drop()
@@ -223,33 +246,9 @@ class TestStorageMongo(unittest.TestCase):
         self.assertEqual(StorageMongoDb._decode_str('hello.world'), 'hello.world')
 
     def test_update_individual_data(self):
-        data = {
-            1: 'integer_value',
-            '1': 'string_value',
-            'dict': {
-                1: 'another_integer_value',
-                '1': {
-                    1: 'last_integer_value',
-                    'something.with.dot': 'another.key.with.dot'
-                }
-            },
-            'something.with.dot': 'key.with.dot',
-            'something.else.with.dots': {
-                'key.dot': 123
-            },
-            'something': {
-                'with.dot': 'nested values',
-                'with': {
-                    'dot': 123
-                }
-            },
-            'li.st': ['is', {'a': 'list', 12: 34}],
-            'tu.ple': (1, 2, 'tuple', (1, 2, 3))
-        }
-
         tag = ['system', 'properties']
-        self.storage.save_data(data, tag)
-        self.assertEqual(data, self.storage.load_data(tag))
+        self.storage.save_data(self.test_individual_data, tag)
+        self.assertEqual(self.test_individual_data, self.storage.load_data(tag))
 
         test_tuple = (1, 2, 3)
         test_list = ['was', {'b': 'string', 24: 68}]
@@ -276,21 +275,48 @@ class TestStorageMongo(unittest.TestCase):
         self.assertEqual(data['dict'], test_dict)
         self.assertEqual(data[1], test_dict)
 
+    def test_update_individual_data_raises_error(self):
+        tag = ['system', 'properties']
+        self.storage.save_data(self.test_individual_data, tag)
+
+        test_dict = {
+            2: 'another_integer_value',
+            '2': {
+                2: 'last_integer_value',
+                'new.with.dot': 'new.key.with.dot'
+            }
+        }
         self.assertRaises(NodeAlreadyExistsError,
-                          self.storage.update_individual_data, 1, test_dict, ['system'])
+                          self.storage.update_individual_data,
+                          1, test_dict, ['system'])
 
         self.assertRaises(NoDataAtKeyError,
-                          self.storage.update_individual_data, 1, test_dict, ['system', 'something'])
+                          self.storage.update_individual_data,
+                          1, test_dict, ['system', 'something'])
 
-        self.assertRaises(Exception, self.storage.update_individual_data,
+        self.assertRaises(NoDataAtKeyError,
+                          self.storage.update_individual_data,
                           1, test_dict, ['something', 'another'])
+
+        self.assertRaises(NodeAlreadyExistsError,
+                          self.storage.update_individual_data,
+                          'a key', test_dict, tag + ['extra'])
+
+    def test_update_individual_data_with_new_key(self):
+        tag = ['system', 'properties']
+        self.storage.save_data(self.test_individual_data, tag)
+
+        test_dict = {
+            2: 'another_integer_value',
+            '2': {
+                2: 'last_integer_value',
+                'new.with.dot': 'new.key.with.dot'
+            }
+        }
 
         self.storage.update_individual_data("new key", test_dict, tag)
         data = self.storage.load_data(tag)
         self.assertEqual(data["new key"], test_dict)
-
-        self.assertRaises(NodeAlreadyExistsError, self.storage.update_individual_data,
-                          'a key', test_dict, tag + ['extra'])
 
     def test_load_individual_data(self):
         data = {
@@ -312,13 +338,37 @@ class TestStorageMongo(unittest.TestCase):
         individual_data = self.storage.load_individual_data('something.dot', tag)
         self.assertEqual(data['something.dot'], individual_data)
 
-        self.assertRaises(NoDataAtKeyError, self.storage.load_individual_data, 1, ['undefined tag'])
-        self.assertRaises(NoDataAtKeyError, self.storage.load_individual_data, 1, ['system'])
-        self.assertRaises(NoDataAtKeyError, self.storage.load_individual_data, 1, [])
+    def test_load_individual_data_raises_error(self):
+        data = {
+            1: 'integer_value',
+            '1.1': 'string_value',
+            'something.dot': {'value': 273, 'unit': 'K'}
+        }
+        tag = ['system', 'properties', 'level 2']
+        self.storage.save_data(data, tag)
 
+        self.assertRaises(NoDataAtKeyError,
+                          self.storage.load_individual_data,
+                          1, ['undefined tag'])
+        self.assertRaises(NoDataAtKeyError,
+                          self.storage.load_individual_data,
+                          1, ['system'])
+        self.assertRaises(NoDataAtKeyError,
+                          self.storage.load_individual_data,
+                          1, [])
         self.assertRaises(NoDataAtKeyError,
                           self.storage.load_individual_data,
                           1, ['system', 'new', 'another'])
 
+    def test_load_individual_data_with_new_key(self):
+        data = {
+            1: 'integer_value',
+            '1.1': 'string_value',
+            'something.dot': {'value': 273, 'unit': 'K'}
+        }
+        tag = ['system', 'properties', 'level 2']
+        self.storage.save_data(data, tag)
+
         self.assertRaises(NoDataAtKeyError,
-                          self.storage.load_individual_data, 'something.dot.NEW', tag)
+                          self.storage.load_individual_data,
+                          'something.dot.NEW', tag)
