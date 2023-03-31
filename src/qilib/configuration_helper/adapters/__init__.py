07e59bc520898c346fd146ee410530c5d10d5481
Bernd Kreynen <b.l.l.kreynen@tudelft.nl>
2023-03-31 12:22:23 +0200

    [SDS-867] Update for Python 3.11 by removing zhinst

--- src/qilib/configuration_helper/adapters/__init__.py
+++ src/qilib/configuration_helper/adapters/__init__.py
@@ -7,10 +7,7 @@ from qilib.configuration_helper.adapters.spi_rack_instrument_adapter import SpiR
 from qilib.configuration_helper.adapters.spi_module_instrument_adapter import SpiModuleInstrumentAdapter
 from qilib.configuration_helper.adapters.d5a_instrument_adapter import D5aInstrumentAdapter
 from qilib.configuration_helper.adapters.f1d_instrument_adapter import F1dInstrumentAdapter
-from qilib.configuration_helper.adapters.hdawg8_instrument_adapter import ZIHDAWG8InstrumentAdapter
 from qilib.configuration_helper.adapters.m2j_instrument_adapter import M2jInstrumentAdapter
 from qilib.configuration_helper.adapters.m4i_instrument_adapter import M4iInstrumentAdapter
 from qilib.configuration_helper.adapters.s5i_instrument_adapter import S5iInstrumentAdapter
-if sys.version_info < (3, 10):
-    from qilib.configuration_helper.adapters.uhfli_instrument_adapter import ZIUHFLIInstrumentAdapter
 from qilib.configuration_helper.adapters.keysight_e8267d_instrument_adapter import KeysightE8267DInstrumentAdapter
